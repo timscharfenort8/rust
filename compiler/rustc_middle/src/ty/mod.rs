499776b367b4f8ad8c69ad4ba8c886e48da14b9b
Jacob Pratt <jacob@jhpratt.dev>
2022-07-22 00:11:31 -0400

    Lower struct field mut restriction to MIR

--- compiler/rustc_middle/src/ty/mod.rs
+++ compiler/rustc_middle/src/ty/mod.rs
@@ -1984,6 +1984,7 @@ pub struct FieldDef {
     pub did: DefId,
     pub name: Symbol,
     pub vis: Visibility<DefId>,
+    pub mut_restriction: Restriction,
 }
 
 impl PartialEq for FieldDef {
@@ -1996,9 +1997,9 @@ impl PartialEq for FieldDef {
         // of `FieldDef` changes, a compile-error will be produced, reminding
         // us to revisit this assumption.
 
-        let Self { did: lhs_did, name: _, vis: _ } = &self;
+        let Self { did: lhs_did, name: _, vis: _, mut_restriction: _ } = &self;
 
-        let Self { did: rhs_did, name: _, vis: _ } = other;
+        let Self { did: rhs_did, name: _, vis: _, mut_restriction: _ } = other;
 
         let res = lhs_did == rhs_did;
 
@@ -2024,7 +2025,7 @@ impl Hash for FieldDef {
         // of `FieldDef` changes, a compile-error will be produced, reminding
         // us to revisit this assumption.
 
-        let Self { did, name: _, vis: _ } = &self;
+        let Self { did, name: _, vis: _, mut_restriction: _ } = &self;
 
         did.hash(s)
     }
