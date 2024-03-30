45f351460334808c137d1cc026af026618712215
Urgau <urgau@numericable.fr>
2024-03-30 16:00:28 +0100

    Add `#[rustc_no_implicit_autorefs]` and apply it to std methods

--- library/alloc/src/string.rs
+++ library/alloc/src/string.rs
@@ -1753,6 +1753,7 @@ impl String {
     #[must_use]
     #[stable(feature = "rust1", since = "1.0.0")]
     #[rustc_confusables("length", "size")]
+    #[cfg_attr(not(bootstrap), rustc_no_implicit_autorefs)]
     pub fn len(&self) -> usize {
         self.vec.len()
     }
@@ -1771,6 +1772,7 @@ impl String {
     #[inline]
     #[must_use]
     #[stable(feature = "rust1", since = "1.0.0")]
+    #[cfg_attr(not(bootstrap), rustc_no_implicit_autorefs)]
     pub fn is_empty(&self) -> bool {
         self.len() == 0
     }
